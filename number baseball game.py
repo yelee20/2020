061fb069c34635e2f4b587bc834fff4991ce691e
Yelee <yelee2369@gmail.com>
2020-04-16 21:58:19 +0800

    add instructions

--- number baseball game.py
+++ number baseball game.py
@@ -4,7 +4,7 @@ import random
 computer_num = random.sample(range(0,9),3)
     
 #get input (three integers) from users
-input_num = list(map(int,input("Guess three different integers (range: 0~9)").split()))[:3]
+input_num = list(map(int,input("Guess three different integers (range: 0~9) \n ex) 3 6 7").split()))[:3]
 
 """>>>>>>>>filter invalid inputs
 for i in range (0,2):
@@ -12,7 +12,7 @@ for i in range (0,2):
         break
     print("You have duplicates! Try again!")"""
 
-
+print(computer_num)
 #Initialize number of balls and strikes
 B=0 #number of balls
 S=0 #number of strikes
@@ -27,17 +27,20 @@ for i in range (0,3):
 
 #Check if the user get same numbers or not
 while S != 3:
-    print("Your input was ",input_num,"\n")
-    print("You got ",B,"Ball(s)\n")
-    print("You got ",S,"Strike(s)")
+    if B==0 and S==0:
+        print("Out!")
+    else:
+        print("Your input was ",input_num,"\n")
+        print("You got ",B,"Ball(s)\n")
+        print("You got ",S,"Strike(s)")
     
     input_num=[]
-    input_num = list(map(int,input("Guess three different integers (range: 0~9)").split()))[:3]
+    input_num = list(map(int,input("Guess three different integers (range: 0~9) \n ex) 3 6 7").split()))[:3]
 
     #initialize number of balls and strikes
     B=0 #number of balls
     S=0 #number of strikes
-
+    
     for i in range (0,3):
         if computer_num[i] == input_num[i]:
             S += 1
@@ -46,5 +49,3 @@ while S != 3:
                 B += 1
 else:
     print("Three strikes!")
-
-    
