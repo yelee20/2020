57b15aeead517fb40b2087dd940d98a689a4eced
yewon lee <yelee2369@gmail.com>
2020-06-06 09:21:50 +0900

    upload saturday study

---
+++ saturday-study/Find greater number.py
@@ -0,0 +1,18 @@
+# No. 2
+#Write a function called findGreaterNumbers which accepts an array and returns the number of times a number is followed by a larger number. 
+#Note that the numbers don't need to be next to each other in the array. 
+#Any pair where the second number comes later in the array and is also the larger number should count.
+
+def findGreaterNumbers(arr):
+  count=0
+  for i in range(len(arr)):
+    for j in range(len(arr)):
+      if arr[i]<arr[j] and i<j:
+        count+=1
+
+  print(count)
+      
+print(findGreaterNumbers([1,2,3])) # 3 (2 > 1, 3 > 2, and 3 > 1)
+print(findGreaterNumbers([6,1,2,7])) # 4
+print(findGreaterNumbers([5,4,3,2,1])) # 0
+print(findGreaterNumbers([])) # 0
---
+++ saturday-study/Find the duplicate.py
@@ -0,0 +1,21 @@
+# No. 1
+#Write a function called findTheDuplicate which accepts an array of numbers containing a single duplicate. 
+#The function returns the number which is a duplicate or undefined if there are no duplicates.
+
+
+def findTheDuplicate(arr):
+    duplicate=[]
+
+    for i in range(len(arr)):
+      for j in range(len(arr)):
+       if arr[i] == arr[j] and i!=j:
+        duplicate.append(arr[i])
+
+    if len(duplicate)==0:
+       print("undefined")
+    else:
+       print(*set(duplicate))
+
+print(findTheDuplicate([1,2,1,4,3,12])) #1
+print(findTheDuplicate([6,1,9,5,3,4,9])) #9
+print(findTheDuplicate([2,1,3,4])) #undefined
